40516eacb9f2f3827f476cd4d71f39a2c84d6d95
Nick Chamberlain <nick.chamberlain.jr@gmail.com>
2014-10-22 20:32:01 -0400

    Starting with arithmetic

---
+++ arithmetic/arithmetic.go
@@ -0,0 +1,22 @@
+
+// 2.01 (**) Determine whether a given integer number is prime.
+// Example:
+// ?- is_prime(7).
+// Yes
+
+// 2.02 (**) Determine the prime factors of a given positive integer.
+// Construct a flat list containing the prime factors in ascending order.
+// Example:
+// ?- prime_factors(315, L).
+// L = [3,3,5,7]
+
+// 2.03 (**) Determine the prime factors of a given positive integer (2).
+// Construct a list containing the prime factors and their multiplicity.
+// Example:
+// ?- prime_factors_mult(315, L).
+// L = [[3,2],[5,1],[7,1]]
+
+// Hint: The solution of problem 1.10 may be helpful.
+
+// 2.04 (*) A list of prime numbers.
+// Given a range of integers by its lower and upper limit, construct a list of all prime numbers in that range.
